1d2272cdc90a13372906dcb7d3ee783efc31e050
joshuawong <joshuawong556@gmail.com>
2015-12-03 11:12:33 -0500

    Create leetcode153.cpp

---
+++ leetcode153.cpp
@@ -0,0 +1,13 @@
+//暴力求解
+class Solution {
+public:
+    int findMin(vector<int>& nums) {
+        int min = nums[0];
+        for(int i = 0; i < nums.size();++i)
+        {
+            if(min > nums[i])
+                min = nums[i];
+        }
+        return min;
+    }
+};
