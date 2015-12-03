c3e9ce12b2fb03b03dbf5f9632e32dde63d64901
joshuawong <joshuawong556@gmail.com>
2015-12-03 11:11:27 -0500

    Create leetcode287.cpp

---
+++ leetcode287.cpp
@@ -0,0 +1,24 @@
+//二分查找法。
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int low = 1;
+        int high = nums.size() - 1;
+        int mid;
+        while(low < high)
+        {
+        	mid = (low + high) / 2;
+        	int count = 0;
+        	for(int num:nums)
+        	{
+        		if(num <= mid)
+        			count++;
+        	}
+        	if(count > mid) 
+        		high = mid;
+        	else
+        		low = mid+1;
+        }
+        return low;
+    }
+};
