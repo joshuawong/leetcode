deab572a6747179caf3f28feb7581f7bc3f6e73d
joshuawong <joshuawong556@gmail.com>
2016-01-13 14:51:33 -0500

    Create leetcode80.cpp

---
+++ leetcode80.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int count = 0;
+        for (int i = 0; i < nums.size(); ++i)
+        {
+        	if(count < 2 || nums[i] > nums[count - 2])
+        	{
+        		nums[count] = nums[i];
+        		count++;
+        	}	
+
+        }
+        return count;
+    }
+};
