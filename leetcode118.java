4c897284ba6dcd6e0c2b8f43e6e1cec21ba5e30d
joshuawong <joshuawong556@gmail.com>
2016-02-22 22:33:40 -0500

    Create leetcode118.java

---
+++ leetcode118.java
@@ -0,0 +1,20 @@
+public class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        
+        for(int i = 0; i < numRows; i++){
+        	ArrayList<Integer> row = new ArrayList<Integer>();
+        	for(int j = 0; j <= i;j++){
+        		if(j == 0 || j == i){
+        			row.add(1);
+        		}
+        		else{
+        			row.add( res.get(i-1).get(j-1) + res.get(i-1).get(j));
+        		}
+        		
+        	}
+        	res.add(row);
+        }
+        return res;
+    }
+}
