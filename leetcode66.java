1a3fed23837842d4a60b761baa1c0bb8dde64586
joshuawong <joshuawong556@gmail.com>
2016-02-22 22:33:28 -0500

    Create leetcode66.java

---
+++ leetcode66.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public int[] plusOne(int[] digits) {
+        int len = digits.length;
+        for(int i = len-1; i >= 0; i--){
+        	if(digits[i] < 9){
+        		digits[i]++;
+        		return digits;
+        	}
+        	digits[i] = 0;
+        }
+        int[] res = new int[len+1];
+        res[0] = 1;
+        return res;
+    }
+}
