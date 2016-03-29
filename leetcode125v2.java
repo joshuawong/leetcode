54957bdc37f281c3235567bcab4921107ef64c6b
joshuawong <joshuawong556@gmail.com>
2016-03-29 10:52:02 -0400

    Create leetcode125v2.java

---
+++ leetcode125v2.java
@@ -0,0 +1,22 @@
+/*
+Solution: two pointers
+Time: O(n)
+Space: O(1)
+*/
+public class Solution {
+    public boolean isPalindrome(String s) {
+        int i = 0;
+        int j = s.length() - 1;
+        while(i < j){
+            while(i < j && !Character.isLetterOrDigit(s.charAt(i)))
+                i++;
+            while(i < j && ! Character.isLetterOrDigit(s.charAt(j)))
+                j--;
+            if(Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j)))
+                return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+}
