73192e23b3905c788711ad3362580e7b08fb9aa6
joshuawong <joshuawong556@gmail.com>
2016-03-31 11:47:59 -0400

    Create leetcode63v2.java

---
+++ leetcode63v2.java
@@ -0,0 +1,31 @@
+/*
+Solution: DP
+Time: O(mn)
+Sapce: O(1)
+*/
+public class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n;j++){
+                if(obstacleGrid[i][j] == 1){
+                    obstacleGrid[i][j] = 0;
+                }
+                else if(i == 0 && j == 0){
+                    obstacleGrid[i][j] = 1;
+                }
+                else if(i == 0){
+                    obstacleGrid[i][j] = obstacleGrid[i][j-1];
+                }
+                else if(j == 0){
+                    obstacleGrid[i][j] = obstacleGrid[i-1][j];
+                }
+                else{
+                    obstacleGrid[i][j] = obstacleGrid[i-1][j] + obstacleGrid[i][j-1];
+                }
+            }
+        }
+        return obstacleGrid[m-1][n-1];
+    }
+}
