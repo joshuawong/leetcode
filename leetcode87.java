91e0a544f68a7c16d106a78f1c147165e297ed7e
joshuawong <joshuawong556@gmail.com>
2016-04-23 14:47:13 -0400

    Create leetcode87.java

---
+++ leetcode87.java
@@ -0,0 +1,28 @@
+public class Solution {
+    public boolean isScramble(String s1, String s2) {
+        if(s1.equals(s2))
+            return true;
+        if(s1.length() != s2.length())
+            return false;
+        
+        int[] letters = new int[256];
+        for(int i = 0; i < s1.length(); i++){
+            letters[s1.charAt(i)]++;
+            letters[s2.charAt(i)]--;
+        }
+        for(int i = 0; i< 256; i++){
+            if(letters[i] != 0)
+                return false;
+        }
+        
+        for(int i = 1; i < s1.length(); i++){
+            if (isScramble(s1.substring(0,i), s2.substring(0,i)) 
+             && isScramble(s1.substring(i), s2.substring(i))) 
+                return true;
+            if (isScramble(s1.substring(0,i), s2.substring(s2.length()-i)) 
+             && isScramble(s1.substring(i), s2.substring(0,s2.length()-i))) 
+                return true;
+        }
+        return false;
+    }
+}
