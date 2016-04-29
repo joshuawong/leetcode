4671f60041d8c282383994fdb0e62340b1b5c0ca
joshuawong <joshuawong556@gmail.com>
2016-04-29 00:05:32 -0400

    Create leetcode343v2.java

---
+++ leetcode343v2.java
@@ -0,0 +1,17 @@
+/*
+Solution: Math
+Time: O(lgn)
+*/
+public class Solution {
+    public int integerBreak(int n) {
+        if(n == 2)
+            return 1;
+        else if(n == 3)
+            return 2;
+        else if(n % 3 == 0)
+            return (int)Math.pow(3,n/3);
+        else if(n % 3 == 1)
+            return (int)Math.pow(3,(n-4)/3) * 2 * 2;
+        else return (int)Math.pow(3, n/3) * 2;
+    }
+}
